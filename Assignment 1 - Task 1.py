3691c22f08632dc65377f6b8543bd06b67aa9ace
Shabad2507 <86426156+Shabad2507@users.noreply.github.com>
2025-04-11 00:40:08 +0530

    Add files via upload

---
+++ Assignment 1 - Task 1.py
@@ -0,0 +1,12 @@
+x = input("Enter the first number:")
+x = float(x)
+y = input("Enter the second number:")
+y = float(y)
+Addition = x + y
+print("Addition:", Addition)
+Subtraction = x - y
+print("Subtraction:", Subtraction)
+Multiplication = x * y
+print("Multiplication:", Multiplication)
+Division = x / y
+print("Division:", Division)
---
+++ Assignment 1 - Task 2.py
@@ -0,0 +1,3 @@
+x = input("Enter your first name:")
+y = input("Enter you last name:")
+print("Hello", x, y + " Welcome to the Python Program.")
